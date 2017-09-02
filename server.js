b69f5435b3a24f29d735f55af11c9b46b99a8522
123varsha <31466657+123varsha@users.noreply.github.com>
2017-09-02 18:37:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-  res.send('article one will be served here');
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two', function (req, res) {
   res.send('article two will be seerved here');
