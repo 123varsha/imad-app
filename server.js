3e8e8bc70111b43033ea901de61e8ec78eae5737
123varsha <31466657+123varsha@users.noreply.github.com>
2017-09-04 20:05:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,6 +88,7 @@ function createtemplate(data)
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
 var counter=0;
 app.get('/counter',function(req,res){
     counter=counter + 1;
