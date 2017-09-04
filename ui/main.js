a4cd783e5c4d35e228fc36c58555507b74e72672
123varsha <31466657+123varsha@users.noreply.github.com>
2017-09-04 19:30:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,3 @@ var element = document.getElementById('main-text');
 element.innerHTML='new value';
 
 
-var img = document.getElementById('madi');
-img.onclick = function () {
-    img.style.marginleft='100px';
-};
